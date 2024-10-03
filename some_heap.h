fb150b743f5d6f878cf61d67f3a1210eb267b4b6
ewinter2 <elliewinter3@gmail.com>
2024-10-02 18:37:36 -0700

    finished!

--- some_heap.h
+++ some_heap.h
@@ -1,12 +1,17 @@
 
-#ifndef HEAP_H
-#define HEAP_H
+#ifndef SOME_HEAP_H
+#define SOME_HEAP_H
 
 // a min heap
 
 typedef unsigned long long heap_key_t;
-typedef void* heap_value_t;
+//typedef void* heap_value_t;
 #define HEAP_KEY_FORMAT "%llu"
+
+typedef union heap_val {
+    unsigned long long as_int;
+    void* as_ptr;
+} heap_value_t;
 
 typedef struct {
     heap_key_t key;
@@ -25,10 +30,24 @@ void heap_free(heap_t *heap);
 
 unsigned int heap_size(heap_t *heap);
 
+unsigned int heap_parent(unsigned int index);
+
+unsigned int heap_left_child(unsigned int index);
+
+unsigned int heap_right_child(unsigned int index);
+
+unsigned int heap_level(unsigned int index);
+
+void heap_print(heap_t *heap);
+
+void heap_swap(heap_t *heap, int index1, int index2);
+
+void heap_bubble_up(heap_t *heap, int index);
+
+void heap_bubble_down(heap_t *heap, int index);
+
 void heap_insert(heap_t *heap, heap_key_t key, heap_value_t data);
 
 heap_value_t heap_remove_min(heap_t *heap);
-
-void heap_print(heap_t *heap);
 
 #endif
